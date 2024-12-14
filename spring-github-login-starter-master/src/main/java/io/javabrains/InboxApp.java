81d6be5a6006b4220e0f716b90b0eebbcfa61863
farabi23 <78350272+farabi23@users.noreply.github.com>
2024-12-14 18:43:34 +0500

    added cassandra db connection config

--- spring-github-login-starter-master/src/main/java/io/javabrains/InboxApp.java
+++ spring-github-login-starter-master/src/main/java/io/javabrains/InboxApp.java
@@ -1,11 +1,16 @@
 package io.javabrains;
 
+import com.datastax.oss.driver.api.core.CqlSessionBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.Path;
 
 @SpringBootApplication
 @RestController
@@ -15,12 +20,15 @@ public class InboxApp {
 		SpringApplication.run(InboxApp.class, args);
 	}
 
-	@RequestMapping("/user")
-	public String user(@AuthenticationPrincipal OAuth2User principal) {
-		System.out.println(principal);
-		return principal.getAttribute("name");
 
-
+	/*
+	This is necessary to use the Astar DB secure with SPring Boot
+	to connect
+	 */
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties){
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
 	}
 	
 
